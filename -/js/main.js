abfa04841b0eaaa74c7f21ff9134156992f1c906
TMMC <TMMC@users.noreply.github.com>
2023-04-10 21:04:32 +0200

    Assets and page files

---
+++ -/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  //== html & body to variable
+  var root = $("html"),
+      body = $("body");
+
+  //== replace html "no-js" class with "js"
+  root.attr("class","js");
+
+  //== initialize tooltips
+  //$('[data-toggle="tooltip"]').tooltip();
+
+});
